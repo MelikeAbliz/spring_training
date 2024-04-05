a5b619989ad0c4e83fedaed7f8550583afbf4b3b
MelikeAbliz <medine20171121@gmail.com>
2024-04-06 00:39:37 +0300

    MVC-RequestParam-PathVariable

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,21 +1,46 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Mentor;
+import com.cydeo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
 
 @Controller
 public class StudentController {
     @RequestMapping("/welcome")
     public String homePage(Model model){//        method model interface, I want to execute method belong to interface
         //static-> put never changes html files,ex: image,javaScrip,css
-//        template-> put hold data , change files
+//        template-> migrate the data and create the view showing the user interface
 //        model-> carry data to view by using addAttribute method
-        //view->thymeleaf
+        //view->thymeleaf,html
 
 
         model.addAttribute("name","Cydeo");
         model.addAttribute("course","MVC");
+        String subject="Spring Boot";
+        model.addAttribute("subject",subject);
+        int studentId= new Random().nextInt();
+        model.addAttribute("id",studentId);
+        List<Integer> numbers=new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(8);
+        model.addAttribute("numbers",numbers);
+        LocalDate dt=LocalDate.now();
+        model.addAttribute("date",dt);
+
+        Student student=new Student(1,"melike","Abliz");
+        model.addAttribute("student",student);
+
         return "student/welcome";
     }
 }
